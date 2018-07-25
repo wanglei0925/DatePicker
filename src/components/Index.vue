4921d2232671abc528e4bc65f364c9af99a760d8
wanglei0925 <15737952566@163.com>
2018-07-25 14:45:51 +0800

    all

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <date-picker></date-picker>
+  </div>
+</template>
+
+<script>
+import DatePicker from "./DatePicker/DatePicker";
+export default {
+  data () {
+    return {
+      msg: '我的时间选择器'
+    }
+  },
+  components:{
+    DatePicker,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
